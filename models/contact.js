07a7aeb714c9a2482b8aa044ad3f0adf569557bf
AlexTanetsDev <alexTanetsDev@gmail.com>
2023-02-28 23:01:09 +0200

    add helpers, midlewares, connect db

---
+++ models/contact.js
@@ -0,0 +1,52 @@
+const { Schema, model } = require("mongoose");
+const Joi = require("joi");
+
+const { handleMongooseError } = require("../helpers");
+
+const contactSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Set name for contact"],
+    },
+    email: {
+      type: String,
+    },
+    phone: {
+      type: String,
+    },
+    favorite: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { versionKey: false, timestamps: true }
+);
+
+contactSchema.post("save", handleMongooseError);
+const emailPattern =
+  /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+const phonePattern = /^(\+38|38)?0\d{9}$/;
+
+const addSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().pattern(emailPattern).required(),
+  phone: Joi.string().pattern(phonePattern).required(),
+  favorite: Joi.boolean(),
+});
+
+const updateFavoriteSchema = Joi.object({
+  favorite: Joi.boolean().required(),
+});
+
+const schemas = {
+  addSchema,
+  updateFavoriteSchema,
+};
+
+const Contact = model("contact", contactSchema);
+
+module.exports = {
+  Contact,
+  schemas,
+};
