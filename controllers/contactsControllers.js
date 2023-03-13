8f7b976e5d99a53745ad3bff85352ec09f7663db
AlexTanetsDev <alexTanetsDev@gmail.com>
2023-03-13 23:33:17 +0200

    add authentification, pagination, sortingByFavorite

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -3,7 +3,26 @@ const { Contact } = require("../models/contact");
 const { HttpError, controllersWrapper } = require("../helpers");
 
 const getAll = async (req, res) => {
-  const result = await Contact.find({}, "-createdAt -updatedAt");
+  const { _id: owner } = req.user;
+  const { page = 1, limit = 10, favorite } = req.query;
+  const skip = (page - 1) * limit;
+  if (favorite) {
+    const result = await Contact.find(
+      { owner, favorite },
+      "-createdAt -updatedAt",
+      {
+        skip,
+        limit,
+      }
+    );
+    res.json(result);
+    return;
+  }
+
+  const result = await Contact.find({ owner }, "-createdAt -updatedAt", {
+    skip,
+    limit,
+  });
   res.json(result);
 };
 
@@ -17,7 +36,8 @@ const getById = async (req, res) => {
 };
 
 const add = async (req, res) => {
-  const result = await Contact.create(req.body);
+  const { _id: owner } = req.user;
+  const result = await Contact.create({ ...req.body, owner });
   res.status(201).json(result);
 };
 
